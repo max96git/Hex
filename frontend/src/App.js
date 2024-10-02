3364fd9f58897ef1ae3495b9cab3d74b14285491
[ B ] <119216428+max96git@users.noreply.github.com>
2024-10-02 21:25:24 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,13 @@ import Signup from './components/Signup';
 import Login from './components/Login';
 import Inventory from './components/Inventory';
 import Avatar from './components/Avatar';
+import Homepage from './components/Homepage';  // Import the homepage
 
 const App = () => {
   return (
     <div>
-      <h1>Hex</h1>
       <Switch>
+        <Route path="/" exact component={Homepage} />  {/* Homepage route */}
         <Route path="/signup" component={Signup} />
         <Route path="/login" component={Login} />
         <Route path="/inventory/:userId" component={Inventory} />
