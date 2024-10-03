6edb76ac245fd38988df4e0292ec73ce7a4f3ae8
[ B ] <119216428+max96git@users.noreply.github.com>
2024-10-03 15:03:19 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,23 @@ import Signup from './components/Signup';
 import Login from './components/Login';
 import Inventory from './components/Inventory';
 import Avatar from './components/Avatar';
-import Homepage from './components/Homepage';  // Import the homepage
+import Homepage from './components/Homepage';
+import GamesPage from './components/GamesPage';      // Import Games Page
+import GameDetails from './components/GameDetails';  // Import Game Details Page
+import CreateGame from './components/CreateGame';    // Import Create Game Page
 
 const App = () => {
   return (
     <div>
       <Switch>
-        <Route path="/" exact component={Homepage} />  {/* Homepage route */}
+        <Route path="/" exact component={Homepage} />
         <Route path="/signup" component={Signup} />
         <Route path="/login" component={Login} />
         <Route path="/inventory/:userId" component={Inventory} />
         <Route path="/avatar/:userId" component={Avatar} />
+        <Route path="/games" component={GamesPage} />        {/* Games Page route */}
+        <Route path="/games/:id" component={GameDetails} />  {/* Game Details route */}
+        <Route path="/create-game" component={CreateGame} /> {/* Create Game route */}
       </Switch>
     </div>
   );
